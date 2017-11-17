00a96d1e6a19d588e2dc29b5fcee989f691b1505
jamescrosbie <jamescrosbie@hotmail.co.uk>
2017-11-17 09:32:52 +0000

    Error catching added

--- scrapeVAOdata.py
+++ scrapeVAOdata.py
@@ -5,17 +5,19 @@ from selenium.webdriver.common.by import By as BY
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup as bs
 import re
+import csv
 import time
 import numpy as np
 
-
-postcodes = ['LS26 0UH'] # 'LS2 7UA', 'SW1A 1AA']
-output_path = '/home/james/Desktop/'
+#postcodes = ['LS26 0UH']#, 'HD3 4AX', 'S17 4PU', 'LS2 7UA', 'SW1A 1AA']
+output_path = 'C:/Users/James Crosbie/Desktop/'
 delay = 10
 
 
 def init_driver():
-    driver = webdriver.Firefox()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--incognito")
+    driver = webdriver.Chrome(executable_path='C:/Users/James Crosbie/Documents/Projects/webScraping/gekoDrivers/chromedriver.exe', chrome_options=chrome_options)
     driver.wait = WebDriverWait(driver, delay)
     return driver
 
@@ -42,23 +44,17 @@ def write_response_file(myLists):
     for i, j, k, l in zip(myLists[0], myLists[1], myLists[2], myLists[3]):
         myString = myString + str(i) + '\t' + str(j) + '\t' + str(k) +'\t' + str(l) + '\n'
 
-    with open(output_path+"addressCTBands.csv", "a") as text_file:
+    with open(output_path+"addressCTBands2.csv", "a") as text_file:
         text_file.write(myString)
     counter += 1
     return
 
 
-def write_error_log(s):
-    with open(output_path+"errors.csv", "a") as text_file:
-        text_file.write(str(s) + '\n')
-    return
-
-
 def readfile():
-    f = open('C:/Users/James Crosbie/Desktop/landreg.html')
-    data = f.read()
-    f.close()
-    return data
+    with open('C:/Users/James Crosbie/Desktop/allpostcodes2.txt', 'r') as f:
+        reader = csv.reader(f)
+        your_list = list(reader)
+    return your_list[0]
 
 
 def stripRegEx(myList):
@@ -82,7 +78,6 @@ def parsePage(soup):
         if improvmentIndicatorLink in str(v.contents):
             impIndList[-1] = 1
 
-
     refList = [str(v.string).replace(" ", "") for i, v in enumerate(tagList) if i in range(3, 100, 4)]
 
     #remove any regular expressions from entries
@@ -92,50 +87,74 @@ def parsePage(soup):
     return addressList, ctBandList, impIndList, refList
 
 
-def main():
-    global counter
-    counter = 1
-    #postcodes = readfile()
-    driver = init_driver()
-    for query in postcodes:
-        page = lookup(driver, query)
+def write_error_log(s, mytype="timeout"):
+    if mytype == 'missing':
+        with open(output_path+"missingPostCodes.csv", "a") as text_file:
+            text_file.write(str(s) + '\n')
+    else:
+        with open(output_path+"timeOutErrors.csv", "a") as text_file:
+            text_file.write(str(s) + '\n')
+    return
 
-        #wait for page to load
-        try:
-            WebDriverWait(driver, delay).until(EC.presence_of_element_located((BY.CLASS_NAME, 'scl_complex')))
-        except TimeoutException:
-            write_error_log(query)
-            continue
 
-        try:
-            driver.find_element_by_link_text("Next page")
-            while driver.find_element_by_link_text("Next page"):
-                #parse page
-                soup = bs(driver.page_source, 'lxml')
-                addressList, ctBandList, impIndList, refList = parsePage(soup)
-                #add to ouput file
-                write_response_file([addressList, ctBandList, impIndList, refList])
-                #move to next page
-                driver.find_element_by_link_text("Next page").click()
+def get_data(driver, query):
+    #wait for page to load
+    try:
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((BY.CLASS_NAME, 'scl_complex')))
+    except:
+        write_error_log(query)
 
-                #wait for page to load
-                try:
-                    WebDriverWait(driver, delay).until(EC.presence_of_element_located((BY.CLASS_NAME, 'scl_complex')))
-                except TimeoutException:
-                    write_error_log(query)
-                    continue
-        except:
+    try:
+        driver.find_element_by_link_text("Next page")
+        while  driver.find_element_by_link_text("Next page"):
             #parse page
             soup = bs(driver.page_source, 'lxml')
             addressList, ctBandList, impIndList, refList = parsePage(soup)
             #add to ouput file
             write_response_file([addressList, ctBandList, impIndList, refList])
+            #move to next page
+            driver.find_element_by_link_text("Next page").click()
+            #wait for page to load
 
-        #wait random time so as not to overload server
-        time.sleep(np.random.randint(2, 6))
+            try:
+                WebDriverWait(driver, delay).until(EC.presence_of_element_located((BY.CLASS_NAME, 'scl_complex')))
+            except:
+                write_error_log(query)
+
+    except:
+        #check for data in page
+        resultSearch = driver.find_element_by_xpath("//div[@id='Content']/div/div/h2").text
+        if resultSearch != 'Search for your Council Tax band - no results':
+            #parse page
+            soup = bs(driver.page_source, 'lxml')
+            addressList, ctBandList, impIndList, refList = parsePage(soup)
+            #add to ouput file
+            write_response_file([addressList, ctBandList, impIndList, refList])
+        else:
+            write_error_log(query, 'missing')
+    return
+
+
+def main():
+    global counter
+    counter = 2
+    postcodes = readfile()
+    driver = init_driver()
+
+    for query in postcodes:
+        query = query.replace("'", "")
+        try:
+            page = lookup(driver, query)
+            get_data(driver, query)
+        except:
+            driver.quit()
+            time.sleep(delay)
+            driver = init_driver()
+
+        #sleep random time
+        time.sleep(np.random.randint(1, 3))
 
     driver.quit()
-
 
 #run the web-scraper
 main()
